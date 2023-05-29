966a14d4e1ffbd6f67724f2333c91f567945b7d5
KTH <KTH@DESKTOP-F52BPRM>
2023-05-29 20:38:29 +0900

    23.05.29
    컨트롤러 데이터 입력 확인

--- src/main/java/com/campmall/controller/JoinController.java
+++ src/main/java/com/campmall/controller/JoinController.java
@@ -3,6 +3,9 @@ package com.campmall.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.campmall.vo.CpmMemberVO;
 
 @Controller
 public class JoinController {
@@ -15,4 +18,23 @@ public class JoinController {
 		return "/join/join";
 	}
 	
+	/**
+	 * joinCheck.do
+	 */
+	@RequestMapping(value="/joinCheck.do", method=RequestMethod.POST)
+	public ModelAndView joinCheck(CpmMemberVO vo) {
+		ModelAndView mv = new ModelAndView();
+		//int result = memberService.getJoinResult(vo);
+		
+		/*
+		if(result == 1){
+			mv.addObject("join_result","ok");
+			mv.setViewName("/login/login");
+		}else{
+			mv.setViewName("errorPage");
+		}
+		
+		*/
+		return mv;
+	}
 }
